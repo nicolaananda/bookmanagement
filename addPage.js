a8573f39afd2a0f29c0716b81b5e3386ed638128
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@gmail.com>
2024-04-21 23:57:17 +0700

    Final Submit

--- addPage.js
+++ addPage.js
@@ -8,12 +8,31 @@ form.addEventListener("submit", async (event) => {
   const title = formData.get("title");
   const author = formData.get("author");
   const year = formData.get("year");
+  const datePattern = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-\d{4}$/;
+  if (!datePattern.test(year)) {
+    alert("Invalid date format. Please use DD-MM-YYYY.");
+    return; // Stop the function if the date is invalid
+  }
   const desc = formData.get("desc");
   const publisher = formData.get("publisher");
   const category = formData.get("category");
   const cover = formData.get("cover");
-  await createData(title, desc, author, year, publisher, category, cover);
+  const response = await createData(
+    title,
+    desc,
+    author,
+    year,
+    publisher,
+    category,
+    cover
+  );
 
+  if (response.ok) {
+    alert("Book added successfully!");
+    // Optionally, handle redirection or updating the UI here
+  } else {
+    alert("Book added successfully!");
+  }
   //   console.log({ title, author, year, desc, publisher, category, cover });
 });
 
